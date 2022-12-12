1de104c779f9dfcbd0d3c8031dab7122b1ff765f
ItsMeIshan <65131563+ItsMeIshan@users.noreply.github.com>
2022-12-12 18:45:58 +0530

    added start scripts

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+const env = require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const router = require("./routes/routes");
 const sequelize = require("./db/database");
 const Customer = require("./models/customer");
-// const Customer = require("./models/customer");
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,7 +14,7 @@ sequelize
   .sync()
   .then((result) => {
     console.log("connection success");
-    app.listen(5000, () => {
+    app.listen(PORT, () => {
       console.log("server listening on port 5000");
     });
   })
