c14a77e79ccbc54a9d47168e01f8d936342a3a71
ItsMeIshan <65131563+ItsMeIshan@users.noreply.github.com>
2022-12-13 20:53:06 +0530

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const createCustomer = require("../controllers/customer");
 const router = express.Router();
-router.route("/").get((req, res) => {
+router.route("/start").get((req, res) => {
   res.send("Welcome to the yoga admission");
 });
 router.route("/create").post(createCustomer);
